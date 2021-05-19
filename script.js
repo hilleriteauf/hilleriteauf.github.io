cec12daa504006f4fac1ec3aeb6daa1fa6ed6b2c
hilleriteauf <46034382+hilleriteauf@users.noreply.github.com>
2021-05-19 12:27:46 +0200

    anchors

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-setAnchors();
-
 function setAnchors() {
-    
-    let buttons = document.querySelectorAll("button[href]");
 
-    buttons.forEach(element => {
-        console.log("bouton : ", element);
-        element.addEventListener("click", () => {
-            console.log("scroll !");
-            element.scrollIntoView({
-                behavior: "smooth",
-                block : "start"
-            });
+    let buttons = document.querySelectorAll("button[href],.button[href]");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            let element = document.querySelectorAll(button.getAttribute("href"));
+            if (element.length > 0) {
+                element[0].scrollIntoView({
+                    behavior: "smooth",
+                    block: "start"
+                });
+            }
         });
     });
 
